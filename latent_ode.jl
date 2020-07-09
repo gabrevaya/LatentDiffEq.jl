b7f916802e58da2a9f796d59942dd05ce5ba4a51
jc-audet <jcgagnon74@gmail.com>
2020-07-09 14:15:07 -0400

    New definition of the reconstruction loss function, fixed lots of bugs linked to saving/importing models, implemented forward_pass function and training visualization

--- latent_ode.jl
+++ latent_ode.jl
@@ -5,7 +5,7 @@
 # https://arxiv.org/abs/1806.07366
 # https://arxiv.org/abs/2003.10775
 
-include("create_data.jl")
+# include("create_data.jl")
 
 using OrdinaryDiffEq
 using Base.Iterators: partition
@@ -26,6 +26,8 @@ using MLDataUtils
 using Statistics
 using Zygote
 using Plots
+using CuArrays
+using Distributions
 
 # overload data loader function so that it picks random start times for each
 # sample, of size seq_len
@@ -120,9 +122,19 @@ KL(μ, logσ²) = -logσ²/2f0 + ((exp(logσ²) + μ^2)/2f0) - 0.5f0
 # the calculation via log(var) = log(σ²) is more numerically efficient than through log(σ)
 # KL(μ, logσ) = (exp(2f0 * logσ) + μ^2)/2f0 - 0.5f0 - logσ
 
+# function rec_loss(x, pred_x)
+#     res = Flux.stack(pred_x - x, 3)
+#     sum(mean((res).^2, dims = (2, 3)))
+# end
+
 function rec_loss(x, pred_x)
-    res = Flux.stack(pred_x - x, 3)
-    sum(mean((res).^2, dims = (2, 3)))
+    pred_x_stacked = Flux.stack(pred_x, 3)
+    x_stacked = Flux.stack(x, 3)
+    res = pred_x_stacked - x_stacked
+    res_average = sum(mean((res).^2, dims = (2, 3)))
+    res_diff = diff(pred_x_stacked, dims = 3) - diff(x_stacked, dims = 3)
+    res_diff_average = sum(mean((res_diff).^2, dims = (2, 3)))
+    return res_average + 1000*res_diff_average
 end
 
 # The following rec_loss is faster but Zygote has a problem with it.
@@ -138,23 +150,21 @@ function loss_batch(encoder, decoder, λ, x, device)
     return reconstruction_loss + kl_loss
 end
 
-
-
 # arguments for the `train` function
 @with_kw mutable struct Args
     η = 1e-3                    # learning rate
-    λ = 0.01f0                  # regularization paramater
+    λ = 0.05f0                  # regularization paramater
     batch_size = 256            # batch size
-    seq_len = 201               # sampling size for output
-    epochs = 20                 # number of epochs
+    seq_len = 100               # sampling size for output
+    epochs = 100                 # number of epochs
     seed = 1                    # random seed
     cuda = true                 # use GPU
-    hidden_dim = 10#200         # hidden dimension
-    rnn_input_dim = 10#32       # rnn input dimension
-    rnn_output_dim = 10#32      # rnn output dimension
-    latent_dim = 2#4            # latent dimension
-    hidden_dim_node = 100#200   # hiddend dimension of the neuralODE
-    t_max = 19.95f0              # edge of time interval for integration
+    hidden_dim = 120         # hidden dimension
+    rnn_input_dim = 32       # rnn input dimension
+    rnn_output_dim = 32      # rnn output dimension
+    latent_dim = 4            # latent dimension
+    hidden_dim_node = 200   # hiddend dimension of the neuralODE
+    t_max = 9.95f0              # edge of time interval for integration
     save_path = "output"        # results path
     data_name = "lv_data.bson"  # data file name
     data_var_name = "full_data" # data file name
@@ -216,53 +226,124 @@ function train(; kws...)
             Flux.Optimise.update!(opt, ps, grad)
             # progress meter
             next!(progress; showvalues=[(:loss, loss)])
+
+            visualize_training(encoder, decoder, x, device)
+
         end
 
         model_path = joinpath(args.save_path, "model_epoch_$(epoch).bson")
-        let encoder_θ = cpu(Flux.params(encoder)), decoder_θ = cpu(Flux.params(decoder)), args=struct2dict(args)
-            BSON.@save model_path encoder_θ decoder_θ args
+        let encoder = cpu(deepcopy(encoder)),
+            decoder = cpu(deepcopy(decoder)),
+            args=struct2dict(args)
+
+            BSON.@save model_path encoder decoder args
             @info "Model saved: $(model_path)"
         end
     end
 
     model_path = joinpath(args.save_path, "model.bson")
-    let encoder_θ = cpu(Flux.params(encoder)), decoder_θ = cpu(Flux.params(decoder)), args=struct2dict(args)
-        BSON.@save model_path encoder_θ decoder_θ args
+    let encoder = cpu(deepcopy(encoder)),
+        decoder = cpu(deepcopy(decoder)),
+        args=struct2dict(args)
+
+        BSON.@save model_path encoder decoder args
         @info "Model saved: $(model_path)"
     end
 end
 
+function visualize_training(encoder, decoder, x, device)
+
+    j = rand(1:size(x[1],2))
+    xᵢ = [ x[i][:,j] for i in 1:size(x, 1)]
+
+    μ, logσ², z = reconstruct(encoder, decoder, xᵢ |> device, device)
+    xₐ = Flux.stack(cpu(xᵢ), 2)
+    zₐ = z[1]
+
+    plt = compare_sol(xₐ, zₐ)
+
+    png(plt, "Training_sample.png")
+end
+
+function predict_from_train()
+
+    #GPU config
+    @load "output/model_epoch_1.bson" args
+    if args[:cuda] && has_cuda_gpu()
+        device = gpu
+        @info "Evaluating on GPU"
+    else
+        device = cpu
+        @info "Evaluating on CPU"
+    end
+
+    # Load a random sample from training set
+    @load "lv_data.bson" full_data
+
+    sol = full_data[:,:,rand(1:10000)]
+
+    # Load model
+    input_dim = size(sol, 1)
+    # @load "output/model_epoch_1.bson" encoder decoder_nODE decoder_linear args
+    @load "output/model_epoch_1.bson" encoder decoder
+
+    decoder_new = Decoder(input_dim, args[:latent_dim], args[:hidden_dim], args[:hidden_dim_node], args[:seq_len], args[:t_max], device)
+
+    Flux.loadparams!(decoder_new.neuralODE, Flux.params(decoder.neuralODE))
+    Flux.loadparams!(decoder_new.linear, Flux.params(decoder.linear))
+
+    # Predict within time interval given
+    x = Flux.unstack(reshape(sol, (size(sol, 1),size(sol, 2), 1)), 2)
+    μ, logσ², z = reconstruct(encoder, decoder_new, x |> device, device)
+
+    # Data dimensions manipulation
+    x = dropdims(Flux.stack(x, 2), dims=3)
+    z = dropdims(Flux.stack(z, 2), dims=3)
+
+    # Showing in plot panel
+    plt = compare_sol(x,z)
+
+    png(plt, "prediction_from_train.png")
+end
+
+function compare_sol(x, z)
+
+    plt = plot(x[1,:], color="blue", label="True x")
+    plt = plot!(x[2,:], color="red", label="True y")
+    plt = plot!(z[1,:], color="blue", linestyle=:dot, label="Model x")
+    plt = plot!(z[2,:], color="red", linestyle=:dot, label="Model y")
+
+    plt
+
+end
+
+
 ## Function comparing solution generated from latentODE structure with true solution within the time span
 function predict_within()
 
-    u0 = [1.0, 1.0] # [x, y]
+    u0 = rand(Uniform(1.5, 3.0), 2, 1) # [x, y]
 
-    pᵢ  = [1.5, 1.0, 3.0, 1.0] # [α, β, δ, γ]
-
-    tspan = (0.0,19.95)
+    tspan = (0.0, 9.95)
     tstep = 0.1
 
     sol = Array(solve_prob(u0, pᵢ, tspan, tstep))
 
     # Load model
     input_dim = size(sol, 1)
-    @load "output/model_epoch_1.bson" encoder_θ decoder_θ args
+    @load "output/model_epoch_16.bson" encoder decoder args
 
-    #
-    # # GPU config
-    # if args[:cuda] && has_cuda_gpu()
-    #     device = gpu
-    #     @info "Evaluating on GPU"
-    # else
-    device = cpu
-    @info "Evaluating on CPU"
-    # end
+    if args[:cuda] && has_cuda_gpu()
+        device = gpu
+        @info "Evaluating on GPU"
+    else
+        device = cpu
+        @info "Evaluating on CPU"
+    end
 
-    encoder = Encoder(input_dim, args[:latent_dim], args[:hidden_dim], args[:rnn_input_dim], args[:rnn_output_dim],device)
-    decoder = Decoder(input_dim, args[:latent_dim], args[:hidden_dim], args[:hidden_dim_node], args[:seq_len], args[:t_max], device)
+    decoder_new = Decoder(input_dim, args[:latent_dim], args[:hidden_dim], args[:hidden_dim_node], args[:seq_len], args[:t_max], device)
 
-    Flux.loadparams!(encoder, encoder_θ)
-    Flux.loadparams!(decoder, decoder_θ)
+    Flux.loadparams!(decoder_new.neuralODE, decoder.neuralODE)
+    Flux.loadparams!(decoder_new.linear, decoder.linear)
 
     # Predict within time interval given
     x = Flux.unstack(reshape(sol, (size(sol, 1),size(sol, 2), 1)), 2)
@@ -273,15 +354,13 @@ function predict_within()
     z = dropdims(Flux.stack(z, 2), dims=3)
 
     # Showing in plot panel
-    plt = plot(x[1,:], color="blue", label="True x")
-    plt = plot!(x[2,:], color="red", label="True y")
-    plt = plot!(z[1,:], color="blue", linestyle=:dot, label="Model x")
-    plt = plot!(z[2,:], color="red", linestyle=:dot, label="Model y")
-    display(plt)
+    plt = compare_sol(x, z)
+
+    png(plt, "prediction_outside_train.png")
 
 end
 
-const seq_len = 201
+const seq_len = 100
 
 if abspath(PROGRAM_FILE) == @__FILE__
     train()
