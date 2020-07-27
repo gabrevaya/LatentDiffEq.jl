8753b821af745b93ae21763218036ea9c89f483b
Germán Abrevaya <gabrevaya@df.uba.ar>
2020-07-27 18:15:05 -0300

    unpack all arguments with macro (be aware of possible issues https://mauro3.github.io/Parameters.jl/latest/manual/\#The-type-specific-\(un\)pack-all-macros-\(somewhat-dangerous\)-1) and fix progressive_training

--- model/model_manager.jl
+++ model/model_manager.jl
@@ -3,11 +3,12 @@
 ## Model manager in order to create right model from those available and assign it the right physical system definition
 function initialize_model(args, device)
 
+    @unpack_Args args       # unpack all arguments
     input_dim = Int(0)      # model input size
     ode_dim = Int(0)        # ode solve size
     p_dim = Int(0)          # number of parameter of system
 
-    if args.problem == "lv"
+    if problem == "lv"
 
         ## Problem defined dimension definition
         input_dim = 2
@@ -18,18 +19,18 @@ function initialize_model(args, device)
         func = lv_func
     end
 
-    if args.model_name == "GOKU"
+    if model_name == "GOKU"
 
         # Create model
-        model = Goku(input_dim, args.latent_dim, args.rnn_input_dim, args.rnn_output_dim, args.hidden_dim, ode_dim, p_dim, func, Tsit5(), device)
+        model = Goku(input_dim, latent_dim, rnn_input_dim, rnn_output_dim, hidden_dim, ode_dim, p_dim, func, Tsit5(), device)
 
         # Get parameters
         ps = Flux.params(model.encoder.linear, model.encoder.rnn, model.encoder.rnn_μ, model.encoder.rnn_logσ², model.encoder.lstm, model.encoder.lstm_μ, model.encoder.lstm_logσ², model.decoder.z₀_linear, model.decoder.p_linear, model.decoder.gen_linear)
 
-    elseif args.model_name == "latent_ode"
+    elseif model_name == "latent_ode"
 
         # Create model
-        model = Latent_ODE(input_dim, args.latent_dim, args.hidden_dim, args.rnn_input_dim, args.rnn_output_dim, args.hidden_dim_node, args.seq_len, args.t_span[2], device)
+        model = Latent_ODE(input_dim, latent_dim, hidden_dim, rnn_input_dim, rnn_output_dim, hidden_dim_node, seq_len, t_span[2], device)
 
         # Get parameters
         ps = Flux.params(model.encoder.linear, model.encoder.rnn, model.encoder.μ, model.encoder.logσ²,
--- model_train.jl
+++ model_train.jl
@@ -23,9 +23,9 @@
     ae = 200                    # Annealing factor epoch end
 
     ## Progressive observation training
-    progressive_training = false    # progressive training usage
+    progressive_training = true    # progressive training usage
     obs_seg_num = 200             # number of step to progressive training
-    start_seq_len = 100         # training sequence length at first step
+    start_seq_len = 20         # training sequence length at first step
     full_seq_len = 400          # training sequence length at last step
 
     ## Model dimensions
@@ -52,9 +52,11 @@ function train(; kws...)
     ## Load hyperparameters and GPU config
 
     args = Args(; kws...)
-    args.seed > 0 && Random.seed!(args.seed)
+    @unpack_Args args
 
-    if args.cuda && has_cuda_gpu()
+    seed > 0 && Random.seed!(seed)
+
+    if cuda && has_cuda_gpu()
         device = gpu
         @info "Training on GPU"
     else
@@ -66,14 +68,14 @@ function train(; kws...)
     ## Prepare training data
 
     # load data from bson
-    @load args.data_file_name raw_data
+    @load data_file_name raw_data
     raw_data = Float32.(raw_data)
     input_dim, time_size, observations = size(raw_data)
     train_set, test_set = splitobs(raw_data, 0.9)
     train_set, val_set = splitobs(train_set, 0.9)
 
     # Initialize dataloaders
-    loader_train = DataLoader(Array(train_set), batchsize=args.batch_size, shuffle=true, partial=false)
+    loader_train = DataLoader(Array(train_set), batchsize=batch_size, shuffle=true, partial=false)
     loader_val = DataLoader(Array(val_set), batchsize=size(val_set, 3), shuffle=true, partial=false)
 
     ############################################################################
@@ -85,33 +87,31 @@ function train(; kws...)
     ## Define optimizer
 
     # ADAM optimizer
-    opt = ADAM(args.η)
+    opt = ADAM(η)
 
     ############################################################################
     ## Various definitions
 
-    mkpath(args.save_path)
+    mkpath(save_path)
 
-    seq_step = (args.full_seq_len - args.start_seq_len) / args.obs_seg_num
-    loss_mem = zeros(Float32, args.epochs)  # TODO : implement loss memorization
+    seq_step = (full_seq_len - start_seq_len) / obs_seg_num
+    loss_mem = zeros(Float32, epochs)  # TODO : implement loss memorization
 
     best_val_loss::Float32 = Inf32
     val_loss::Float32 = 0
 
     ############################################################################
     ## Main train loop
-    @info "Start Training of $(args.model_name)-net, total $(args.epochs) epochs"
-    for epoch = 1:args.epochs
+    @info "Start Training of $(model_name)-net, total $(epochs) epochs"
+    for epoch = 1:epochs
 
         ## define seq_len according to training mode (progressive or not)
-        if args.progressive_training
-            seq_len = Int( args.start_seq_len + seq_step * floor( (epoch-1) / (args.epochs/args.obs_seg_num) ) )
-        else
-            seq_len = args.seq_len
+        if progressive_training
+            seq_len = Int( round( start_seq_len + seq_step * floor( (epoch-1) / (epochs/obs_seg_num) ) ) )
         end
 
         # Model evaluation length
-        t = range(args.t_span[1], step=args.dt, length=seq_len)
+        t = range(t_span[1], step=dt, length=seq_len)
 
         af = 0.     # Annealing factor
         mb_id = 1   # Minibatch id
@@ -120,15 +120,15 @@ function train(; kws...)
         for x in loader_train
 
             # Use only a random sequence of length seq_len for all sample in the minibatch
-            x = time_loader(x, args.full_seq_len, seq_len)
+            x = time_loader(x, full_seq_len, seq_len)
 
             # Comput annealing factor
-            af = annealing_factor(args.start_af, args.end_af, args.ae, epoch, mb_id, length(loader_train))
+            af = annealing_factor(start_af, end_af, ae, epoch, mb_id, length(loader_train))
 
             loss, back = Flux.pullback(ps) do
 
                 # Compute loss
-                loss_batch(model, args.λ, x |> device, t, af)
+                loss_batch(model, λ, x |> device, t, af)
 
             end
 
@@ -138,8 +138,8 @@ function train(; kws...)
             Flux.Optimise.update!(opt, ps, grad)
 
             # Use validation set to get loss and visualisation
-            val_set = time_loader(first(loader_val), args.full_seq_len, seq_len)
-            val_loss = loss_batch(model, args.λ, val_set |> device, t, af)
+            val_set = time_loader(first(loader_val), full_seq_len, seq_len)
+            val_loss = loss_batch(model, λ, val_set |> device, t, af)
 
             # progress meter
             next!(progress; showvalues=[(:loss, loss),(:val_loss, val_loss)])
@@ -148,14 +148,14 @@ function train(; kws...)
 
         loss_mem[epoch] = val_loss
         if device != gpu
-            val_set = time_loader(first(loader_val), args.full_seq_len, seq_len)
+            val_set = time_loader(first(loader_val), full_seq_len, seq_len)
             visualize_training(model, val_set |> device, t)
         end
 
         if val_loss < best_val_loss
             best_val_loss = deepcopy(val_loss)
 
-            model_path = joinpath(args.save_path, "best_model_$(args.model_name).bson")
+            model_path = joinpath(save_path, "best_model_$(model_name).bson")
             let model = cpu(model),
                 args=struct2dict(args)
 
