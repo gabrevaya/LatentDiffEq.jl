4301b63c0691c54a0b71e02ff17de7fe741059a9
Germán Abrevaya <gabrevaya@df.uba.ar>
2021-03-09 16:02:00 -0300

    start plugging NN inside ODEs

--- systems/pendulum_NN.jl
+++ systems/pendulum_NN.jl
@@ -3,26 +3,30 @@
 ################################################################################
 ## Problem Definition -- frictionless pendulum
 
-struct pendulum{T,P,F} <: AbstractSystem
+struct pendulum_NN{T,P,F} <: AbstractSystem
     u₀::T
     p::T
     prob::P
     transform::F
 
-    function pendulum()
+    function pendulum_NN()
         # Default parameters and initial conditions
         u₀ = Float32[1.0, 1.0]
-        p = Float32[1.]
         tspan = (0.f0, 1.f0)
+
+        model_univ = FastChain(FastDense(2, 16, tanh),
+                       FastDense(16, 16, tanh),
+                       FastDense(16, 1))
+
+        # The model weights are destructured into a vector of parameters
+        p = initial_params(model_univ)
 
         # Define differential equations
         function f!(du, u, p, t)
                 x, y = u
-                G = 10.0f0
-                L = p[1]
-                
+
                 du[1] = y
-                du[2] =  -G/L*sin(x)
+                du[2] = model_univ(u, p)[1]
         end
 
         output_transform(u) = u
@@ -30,10 +34,11 @@ struct pendulum{T,P,F} <: AbstractSystem
         # Build ODE Problem
         _prob = ODEProblem(f!, u₀, tspan, p)
 
-        @info "Optimizing ODE Problem"
-        sys = modelingtoolkitize(_prob)
-        ODEFunc = ODEFunction(sys, tgrad=true, jac = true, sparse = false, simplify = false)
-        prob = ODEProblem(ODEFunc, u₀, tspan, p)
+        # @info "Optimizing ODE Problem"
+        # sys = modelingtoolkitize(_prob)
+        # ODEFunc = ODEFunction(sys, tgrad=true, jac = true, sparse = false, simplify = false)
+        # prob = ODEProblem(ODEFunc, u₀, tspan, p)
+        prob = _prob
 
         T = typeof(u₀)
         P = typeof(prob)
